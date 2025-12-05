ad87bd52b67d94387e78e4209715658990399b72
Adrians29 <adrian.egl@hotmail.com>
2025-12-04 20:13:00 -0500

    feat: Implement fields and first method

--- src/main/java/org/adrianegl/Address.java
+++ src/main/java/org/adrianegl/Address.java
@@ -1,4 +1,37 @@
 package org.adrianegl;
 
 public class Address {
+    private int streetNo;
+    private String street;
+    private String city;
+    private Province province;
+    private String postalCode;
+
+    public enum Province{
+        AB, BC, MB, NB, NL, NS, ON, PE, QC, SK
+    }
+
+    /**
+     *Checks if a postcode is valid or not
+     * @param postalCode the postcode that is going to be used with a length of 6
+     * @return if the postcode is valid or not
+     */
+    public static boolean isPostalCodeValid(String postalCode) {
+        int len = postalCode.length();
+        if (len != 6) {
+            return false;
+        }
+        for (int i = 0; i < len; i += 2) {
+            char letter = postalCode.charAt(i);
+            char digit = postalCode.charAt(i + 1);
+            if (!Character.isLetter(letter) || !Character.isUpperCase(letter)) {
+                return false;
+            }
+            else if (!Character.isDigit(digit)) {
+                return false;
+            }
+        }
+        return true;
+
+    }
 }
